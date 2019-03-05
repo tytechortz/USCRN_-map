d7f7b02272770bfb0b3e38f3f5fc8d6191740fe9
James <tytechortz@gmail.com>
2019-03-04 18:13:32 -0700

    hovermode and margins

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# df = pd.read_csv(
-#     'https://raw.githubusercontent.com/'
-#     'plotly/datasets/master/'
-#     '1962_2006_walmart_store_openings.csv')
 
 df = pd.read_csv('./all_uscrn_data.txt', delim_whitespace=True, header=None, usecols = [0,1,3,4,5,6,7,8], names =['WBAN','YM','LON','LAT','MAX','MIN','MEAN','AVG',])
 
@@ -35,29 +31,13 @@ app.layout = html.Div([
         'layout': {
             'mapbox': {
                 'accesstoken': 'pk.eyJ1IjoidHl0ZWNob3J0eiIsImEiOiJjanN1emtuc2cwMXNhNDNuejdrMnN2aHYyIn0.kY0fOoozCTY-4IUzcLx22w',
-                'zoom':5
+                'zoom':5,
+                'hovermode': 'closest',
+                'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0},
             },
-            'hovermode': 'closest',
-            'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0},
-            
         }
     })
 ])
-
-
-# @app.callback(
-#     dash.dependencies.Output('text-content', 'children'),
-#     [dash.dependencies.Input('map', 'hoverData')])
-# def update_text(hoverData):
-#     s = df[df['storenum'] == hoverData['points'][0]['customdata']]
-#     return html.H3(
-#         'The {}, {} {} opened in {}'.format(
-#             s.iloc[0]['STRCITY'],
-#             s.iloc[0]['STRSTATE'],
-#             s.iloc[0]['type_store'],
-#             s.iloc[0]['YEAR']
-#         )
-#     )
 
 
 if __name__ == '__main__':
